f3b632ec5f5d5056a160637e20ffdb1dfe79f6a0
James McLaughlin <james@mclgh.net>
2020-07-17 12:44:27 +0100

    add k8 chart

--- oxo-loader/load_all.sh
+++ oxo-loader/load_all.sh
@@ -1,8 +1,10 @@
-python /app/OlsDatasetExtractor.py -c /app/config/config.ini -i /app/config/idorg.xml -d /app/data/datasources.csv
-python /app/OxoNeo4jLoader.py -W -d datasources.csv -c /app/config/config.ini
+#!/usr/bin/env bash
 
-python /app/OlsMappingExtractor.py -c /app/config/config.ini -t /app/data/ols_terms.csv -m /app/data/ols_mappings.csv
-python /app/OxoNeo4jLoader.py -c /app/config/config.ini -t ols_terms.csv -m ols_mappings.csv
+python /opt/oxo-loader/OlsDatasetExtractor.py -c $CONFIG_INI -i $IDORG_XML -d $NEO4JIMPORT/datasources.csv
+python /opt/oxo-loader/OxoNeo4jLoader.py -W -d datasources.csv -c $CONFIG_INI
 
-python /app/UmlsMappingExtractor.py -c /app/config/config.ini -t /app/data/umls_terms.csv -m /app/data/umls_mappings.csv
-python /app/OxoNeo4jLoader.py -c /app/config/config.ini -t umls_terms.csv -m umls_mappings.csv
+python /opt/oxo-loader/OlsMappingExtractor.py -c $CONFIG_INI -t $NEO4JIMPORT/ols_terms.csv -m $NEO4JIMPORT/ols_mappings.csv
+python /opt/oxo-loader/OxoNeo4jLoader.py -c $CONFIG_INI -t ols_terms.csv -m ols_mappings.csv
+
+#python /app/UmlsMappingExtractor.py -c /app/config/config.ini -t $NEO4JIMPORT/umls_terms.csv -m $NEO4JIMPORT/umls_mappings.csv
+#python /app/OxoNeo4jLoader.py -c $CONFIG_INI -t umls_terms.csv -m umls_mappings.csv
